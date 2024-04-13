04d4ff44817ed22e3ab5d40a5f4927570ccea254
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-04-13 18:51:01 +0200

    Add simulation bringup

---
+++ src/diffdrive_bringup/launch/diffdrive_bringup.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    declared_arguments = [
+        DeclareLaunchArgument(
+            "world",
+            default_value="default.sdf",
+            description="Robot controller to start.",
+        ),
+        DeclareLaunchArgument(
+            name="use_sim",
+            default_value="True",
+            description='Set to "true" to run simulation',
+        )
+    ]
+
+    ign_gazebo_launch = PathJoinSubstitution(
+        [FindPackageShare("ros_ign_gazebo"), 'launch', 'ign_gazebo.launch.py'])
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([ign_gazebo_launch]),
+         launch_arguments=[
+            ('ign_args', [LaunchConfiguration('world'),' -v 4'])])
+
+    ign_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="ign_bridge",
+        arguments=[
+            "/clock" + "@rosgraph_msgs/msg/Clock" + "[ignition.msgs.Clock",
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription(declared_arguments + [ign_bridge, gazebo])
