293d90ecf36b7b7807d2a060e079f7446066dec3
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-04-17 20:14:27 +0000

    Update rviz file

--- src/diffdrive_bringup/setup.py
+++ src/diffdrive_bringup/setup.py
@@ -11,6 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
         (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml"))
     ],
